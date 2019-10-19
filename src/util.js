939576084336eda9a54a8dce9c622ebbb2459b83
Joakim Wimmerstedt <joakim.wimmerstedt@gmail.com>
2019-10-19 19:45:15 +0200

    Made convertObjectKeysToLowerCase recursive

--- src/util.js
+++ src/util.js
@@ -1,3 +1,13 @@
 export const convertObjectKeysToLowerCase = obj => Object.fromEntries(
-  Object.entries(obj).map(([k, v]) => [k.toLowerCase(), v])
+  Object.entries(obj).map(([k, v]) => {
+    let value = v;
+    if (Array.isArray(value)) {
+      value = value.map(currentValue =>
+        typeof currentValue === 'object' ? convertObjectKeysToLowerCase(currentValue) : currentValue
+      );
+    } else if (typeof value === 'object') {
+      value = convertObjectKeysToLowerCase(value);
+    }
+    return [k.toLowerCase(), value];
+  })
 );
